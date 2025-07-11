70e8766b66e504a6a4c8462962195bc1a2483aba
Gleb Koshcheev <gleb.koshcheev@gmail.com>
2025-07-11 18:47:50 -0400

    set hook to trigger after:package:cleanup

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ export default class SelectiveFunctionDeploy {
       this.options = options;
       this.log = log;
       this.logPrefix = '[serverless-selective-function-deploy]:';
-  
+
       this.setupFunctionProperties();
       this.setupHooks();
     }
-  
+
     setupHooks() {
       this.hooks = {
-        'before:package:createDeploymentArtifacts': () => this.excludeNonDeployableFunctions(),
+        'after:package:cleanup': () => this.excludeNonDeployableFunctions(),
       };
     }
-  
+
     setupFunctionProperties() {
       this.serverless.configSchemaHandler.defineFunctionProperties('aws', {
         properties: {
@@ -23,25 +23,25 @@ export default class SelectiveFunctionDeploy {
         required: [],
       });
     }
-  
+
     excludeNonDeployableFunctions() {
       const allFunctions = this.getAllFunctions();
-  
+
       if (allFunctions.length === 0) {
         return;
       }
-  
+
       const excludedFunctions = [];
       const toDeployFunctions = [];
-  
+
       for (const functionName of allFunctions) {
         const func = this.getFunction(functionName);
         const { toDeploy = true } = func;
-  
+
         if (typeof toDeploy !== 'boolean') {
           throw new this.serverless.classes.Error('toDeploy property must be a boolean');
         }
-  
+
         if (!toDeploy) {
           this.excludeFunctionFromDeployment(functionName);
           excludedFunctions.push(functionName);
@@ -49,26 +49,26 @@ export default class SelectiveFunctionDeploy {
           toDeployFunctions.push(functionName);
         }
       }
-  
+
       if (excludedFunctions.length > 0 || toDeployFunctions.length > 0) {
         this.log.verbose(`${this.logPrefix} Pre-deployment function summary:`);
         this.log.verbose(`${this.logPrefix} Excluded ${excludedFunctions.length} function(s): ${excludedFunctions.join(', ')}`);
         this.log.verbose(`${this.logPrefix} Included ${toDeployFunctions.length} function(s): ${toDeployFunctions.join(', ')}`);
       }
-  
+
       const summaryLog = `${this.logPrefix} Excluded ${excludedFunctions.length} function(s) from deployment, deploying ${toDeployFunctions.length} of ${allFunctions.length} function(s).`;
       const verbosePrompt = !this.options.verbose ? ' For more details, use --verbose command.' : '';
       this.log.notice(summaryLog + verbosePrompt);
     }
-  
+
     excludeFunctionFromDeployment(functionName) {
       delete this.serverless.service.functions[functionName];
     }
-  
+
     getAllFunctions() {
       return this.serverless.service.getAllFunctions();
     }
-  
+
     getFunction(functionName) {
       return this.serverless.service.getFunction(functionName);
     }
